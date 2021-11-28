78592a9d598461373a1866d7816e237696eeac73
7881717 <88056706+7881717@users.noreply.github.com>
2021-11-28 20:46:34 +0200

    Create Main.java

---
+++ app/src/main/java/com/bigapplication/Main.java
@@ -0,0 +1,9 @@
+package com.bigapplication;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World"+args[0]);
+    }
+}
+
